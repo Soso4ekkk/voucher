8714ba2f92c9c5875e9db998d3df8e8a1a2bd6d8
soso4ekkk <zabolotskikh0371@mail.ru>
2022-05-08 23:31:56 +0400

    added className

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,17 +8,19 @@ function Header(props) {
     return (
         <div>
             {/****************************мем****************************/}
-            <div>
-                <img className="img" src="/images/trusov.png" alt="trusov" />
-            </div>
-            <div className="block_1">
-                <div className="block_2">
-                    <img src="/images/trusov&mushroom.png" alt="trusov&mushroom" />
-                    <p className="block_3">
-                        Я&nbsp;в&nbsp;себя&nbsp;так&nbsp;не&nbsp;верю,
-                        <br></br>как&nbsp;верю&nbsp;в&nbsp;вас.
-                        <br></br><br></br>©&nbsp;А.С.&nbsp;Трусов
-                    </p>
+            <div className="memes">
+                <div>
+                    <img className="img" src="/images/trusov.png" alt="trusov" />
+                </div>
+                <div className="block_1">
+                    <div className="block_2">
+                        <img src="/images/trusov&mushroom.png" alt="trusov&mushroom" />
+                        <p className="block_3">
+                            Я&nbsp;в&nbsp;себя&nbsp;так&nbsp;не&nbsp;верю,
+                            <br></br>как&nbsp;верю&nbsp;в&nbsp;вас.
+                            <br></br><br></br>©&nbsp;А.С.&nbsp;Трусов
+                        </p>
+                    </div>
                 </div>
             </div>
 
