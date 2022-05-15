9f0a39ff1644a7e1f4127c43348a5e2e76a1bfb9
soso4ekkk <zabolotskikh0371@mail.ru>
2022-05-15 20:33:09 +0400

    delete meme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,25 +7,6 @@ function Header(props) {
 
     return (
         <div>
-            {/*************************** мем ***************************/}
-            <div className="memes">
-                <div>
-                    <img className="img" src="/images/trusov.png" alt="trusov" />
-                </div>
-                <div className="block_1">
-                    <div className="block_2">
-                        <img src="/images/trusov&mushroom.png" alt="trusov&mushroom" />
-                        <p className="block_3">
-                            Я&nbsp;в&nbsp;себя&nbsp;так&nbsp;не&nbsp;верю,
-                            <br></br>как&nbsp;верю&nbsp;в&nbsp;вас.
-                            <br></br><br></br>©&nbsp;А.С.&nbsp;Трусов
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            {/***********************************************************/}
-
             <div className="button">
                 <Button
                     onClick={setActiveButton}
