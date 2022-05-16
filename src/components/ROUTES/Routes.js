7b497decf81075d89989a2b22d33895a8c1656c7
soso4ekkk <zabolotskikh0371@mail.ru>
2022-05-16 16:33:45 +0400

    added new fail

---
+++ src/components/ROUTES/Routes.js
@@ -0,0 +1,20 @@
+const ROUTES = {
+    MAIN: {
+        path: '/',
+        title: ''
+    },
+    CALCULATOR: {
+        path: '/calculator',
+        title: 'Калькулятор'
+    },
+    GRAPH2D: {
+        path: '/graph2D',
+        title: 'Графика 2D'
+    },
+    GRAPH3D: {
+        path: '/graph3D',
+        title: 'Графика 3D'
+    }
+}
+
+export default ROUTES;
