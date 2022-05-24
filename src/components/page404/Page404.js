abf36163f515a333819635e69290bd1f4cdff47a
soso4ekkk <zabolotskikh0371@mail.ru>
2022-05-24 21:51:55 +0400

    added new component

---
+++ src/components/page404/Page404.js
@@ -0,0 +1,14 @@
+import './page404.css';
+
+function Page404() {
+    return (
+        <div className="page404">
+            <div className="textError">
+                <p className="textErrorNumber">404</p>
+                <p>not&nbsp;found</p>   
+            </div>
+        </div>
+    );
+}
+
+export default Page404;
